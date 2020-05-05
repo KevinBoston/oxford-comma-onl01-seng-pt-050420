a80ecc639941ed84d5905cf0d22acda401d2067e
Kevin Boston <kbos22@gmail.com>
2020-05-05 12:38:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
-  elsif array.length
-  array.join(",")
+  elsif array.length > 2
+    last_el = array.pop
+    comm_arr = array.join(",")
+    comm_arr << ", and " + last_el
 end
