fd286ad985fa8c2455e131fda3a6875b784343fe
Kevin Boston <kbos22@gmail.com>
2020-05-05 12:40:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length > 2
     last_el = array.pop
-    comm_arr = array.join(",")
+    comm_arr = array.join(" , ")
     comm_arr << ", and " + last_el
   elsif array.length == 1
     array.split
