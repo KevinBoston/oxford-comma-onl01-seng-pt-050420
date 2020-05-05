2c41b640f89353cd55221094a8a5331c2062a96d
Kevin Boston <kbos22@gmail.com>
2020-05-05 12:44:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    last_el = array.pop
+    comm_arr = array.join(", ")
+    comm_arr << ", and " + last_el
+  elsif array.length == 1
+    array.join
+  end
 end
